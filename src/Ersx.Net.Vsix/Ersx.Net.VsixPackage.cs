70b49e5f6257e5b4921e6b5f39dac3cec7fbdaf6
Yonatan Eldar <seldary@gmail.com>
2015-01-08 23:55:24 +0200

    Added feedback through status bar and output window.

--- src/Ersx.Net.Vsix/Ersx.Net.VsixPackage.cs
+++ src/Ersx.Net.Vsix/Ersx.Net.VsixPackage.cs
@@ -19,20 +19,27 @@ namespace seldary.Ersx_Net_Vsix
     public sealed class Ersx_Net_VsixPackage : Package
     {
         private readonly ResxSorter _resxSorter = new ResxSorter();
-
-        #region Package Members
+        private Guid _resxOutputWindowId = new Guid("64FD8E73-DD6E-4697-A20D-B2307205A764");
+        private IVsStatusbar _statusBar;
+        private OleMenuCommandService _oleMenuCommandService;
+        private IVsOutputWindow _outputWindow;
+        private object _sortIcon;
 
         protected override void Initialize()
         {
             base.Initialize();
 
-            var oleMenuCommandService = GetService(typeof (IMenuCommandService)) as OleMenuCommandService;
-            if (oleMenuCommandService != null)
+            _statusBar = GetService(typeof (SVsStatusbar)) as IVsStatusbar;
+            _oleMenuCommandService = GetService(typeof (IMenuCommandService)) as OleMenuCommandService;
+            _outputWindow = GetService(typeof (SVsOutputWindow)) as IVsOutputWindow;
+            _sortIcon = (short) Constants.SBAI_General;
+
+            if (_oleMenuCommandService != null)
             {
                 var menuCommandId = new CommandID(GuidList.guidErsx_Net_VsixCmdSet, (int) PkgCmdIDList.sortResx);
                 var menuItem = new OleMenuCommand(MenuItemClick, menuCommandId);
                 menuItem.BeforeQueryStatus += BeforeContextMenuOpens;
-                oleMenuCommandService.AddCommand(menuItem);
+                _oleMenuCommandService.AddCommand(menuItem);
             }
         }
 
@@ -89,24 +96,53 @@ namespace seldary.Ersx_Net_Vsix
 
         private IEnumerable<string> GetSelectedResxFilePaths()
         {
-            return GetSelectedFileFullPaths().Where(IsResxFile);
+            return GetSelectedFileFullPaths().Where(fullPath =>
+                fullPath.EndsWith(".resx", StringComparison.OrdinalIgnoreCase));
         }
-
-        private bool IsResxFile(string fullPath)
-        {
-            return fullPath.EndsWith(".resx", StringComparison.OrdinalIgnoreCase);
-        }
-
-        #endregion
 
         private void MenuItemClick(object sender, EventArgs e)
         {
-            foreach (var selectedFileFullPath in GetSelectedResxFilePaths())
+            try
             {
-                _resxSorter.
-                    Sort(XDocument.Load(selectedFileFullPath)).
-                    Save(selectedFileFullPath);
+                uint cookie = 0;
+                _statusBar.Progress(ref cookie, 1, "", 0, 0);
+                _statusBar.Animation(1, ref _sortIcon);
+                var selectedResxFilePaths = GetSelectedResxFilePaths().ToList();
+                for (var i = 0; i < selectedResxFilePaths.Count; i++)
+                {
+                    _resxSorter.
+                        Sort(XDocument.Load(selectedResxFilePaths[i])).
+                        Save(selectedResxFilePaths[i]);
+
+                    _statusBar.Progress(ref cookie, 1, "", (uint) i + 1, (uint) selectedResxFilePaths.Count);
+                    _statusBar.SetText(string.Format("Sorting {0} out of {1} resx files...", i + 1, selectedResxFilePaths.Count));
+                }
+
+                WriteToOutput(string.Join(Environment.NewLine, new[] {string.Format("Sorted {0} resx files:", selectedResxFilePaths.Count())}.Concat(selectedResxFilePaths)));
+                _statusBar.Progress(ref cookie, 0, "", 0, 0);
+                _statusBar.Animation(0, ref _sortIcon);
+                SetStatusBar("Resx sort succeeded");
             }
+            catch (Exception exception)
+            {
+                WriteToOutput("Failed to sort resx files: {0}", exception.Message);
+                SetStatusBar("Failed to sort resx files");
+            }
+        }
+
+        private void WriteToOutput(string message, params object[] messageParams)
+        {
+            _outputWindow.CreatePane(ref _resxOutputWindowId, "Resx", 1, 1);
+            IVsOutputWindowPane customPane;
+            _outputWindow.GetPane(ref _resxOutputWindowId, out customPane);
+            customPane.Clear();
+            customPane.OutputString(string.Format(message, messageParams));
+            customPane.Activate();
+        }
+
+        private void SetStatusBar(string message, params object[] messageParams)
+        {
+            _statusBar.SetText(string.Format(message, messageParams));
         }
     }
 }
